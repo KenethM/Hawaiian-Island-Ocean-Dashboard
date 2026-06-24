4c35a29cc1c4c9a9d1a05e4677f127c1077c2c48
KenethM <KenethM@users.noreply.github.com>
2026-06-24 11:21:16 +0000

    deploy: 75b97fc6675317b0b355fd00353bc107407f7b56

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Hawaiian-Island-Ocean-Dashboard/sw.js', { scope: '/Hawaiian-Island-Ocean-Dashboard/' })})}
